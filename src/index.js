4af540a2e4b3946ad0ffea32a875744c29f82b68
mateoap00 <mateo.a.p.2000@gmail.com>
2022-09-07 18:20:06 -0500

    Add toggle button to change order in which the moves are displayed and style in the winner squares

--- src/index.js
+++ src/index.js
@@ -1,15 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import Toggle from 'react-toggle'
 import './index.css';
 
-// Square as a function component
-// renders a button with a given value inside,
+// Square as a function component, the styling will change if the square is part of the
+// winner row of Xs or Os, also renders a button with the given value inside,
 // declares a onClick prop that calls another onClick prop of a parent component.
 function Square(props) {
     return (
         <button
             key={props.keyValue}
-            className="square"
+            className={props.squareClass}
             onClick={props.onClick}
         >
             {props.value}
@@ -17,28 +18,52 @@ function Square(props) {
     );
 }
 
+//Toggle function component to change the order in which the moves show
+// ascending or descending
+function ToggleOrder(props) {
+    return (
+        <div className='toggleButton'>
+            <label>
+                <span>ASCENDING </span>
+                <Toggle
+                    defaultChecked={false}
+                    icons={false}
+                    disabled={!props.enableToggle}
+                    onChange={props.handleToggleChange}
+                />
+                <span> DESCENDING</span>
+            </label>
+
+
+        </div>
+    );
+}
+
 // Board component
 // renders the board with 9 squares in it
 // for that it uses the renderSquare function that returns a Square component
-// with a value and a onClick prop that calls a onClick function of a parent component.
+// with a value and a onClick prop that calls a onClick function of a parent component,
+// also added a squareClass prop that is used to define if the square is part of the
+// winner row or just part of the board.
 class Board extends React.Component {
 
-    renderSquare(i, j, k) {
+    renderSquare(i, j, k, squareClass) {
         return <Square
             keyValue={`square#${i}`}
             value={this.props.squares[i]}
+            squareClass={squareClass}
             onClick={() => this.props.onClick(i, j, k)}
         />;
     }
 
-    renderAll() {
-        //With only one for loop, render all divs 'board-row'
-    }
-
     // Renders the board of squares with two loops, the lower loop is used to generate
     // the squares components of each row and the upper loop is used to generate
-    // each row (a div) with the squares.
+    // each row (a div) with the squares,
+    // also checks if the indexes of the squares that are going to be rendered 
+    // are part of the winner row.
     render() {
+        const rowWinner = this.props.rowWinner;
+        let squareClass = 'square';
         let i = 0;
         let key = '';
         const rows = [];
@@ -46,7 +71,14 @@ class Board extends React.Component {
             const cols = [];
             key = `row#${j}`;
             for (let k = 0; k < 3; k++) {
-                cols.push(this.renderSquare(i, j, k));
+                if (rowWinner) {
+                    if (i === rowWinner[0] || i === rowWinner[1] || i === rowWinner[2]) {
+                        squareClass = 'square won';
+                    } else {
+                        squareClass = 'square';
+                    }
+                }
+                cols.push(this.renderSquare(i, j, k, squareClass));
                 i++;
             }
             rows.push(
@@ -78,13 +110,15 @@ class Game extends React.Component {
             stepNumber: 0,
             stepsLocation: [],
             xIsNext: true,
+            ascending: true,
+            won: false,
+            rowWinner: null,
         };
     }
 
     // handler for a click in any of the buttons of the board
     // takes the number of the button (0-9) and the position in which the button shows (row, col)
     handleClick(i, j, k) {
-        console.log(i);
         // Takes a copy (slice) of the history with all the versions of the squares, if  a back-to-move was made
         // it takes the history up until the selected move (the step).
         const history = this.state.history.slice(0, this.state.stepNumber + 1);
@@ -131,20 +165,30 @@ class Game extends React.Component {
         });
     }
 
+    // Handles the change of the toggle button, used to change the order in which are
+    // rendered the list moves, ascending or descending.
+    handleToggleChange() {
+        console.log('WORKS!');
+        this.setState({
+            ascending: !this.state.ascending,
+        });
+
+    }
+
     //Render method for the game component
     render() {
         //Entire history of the game (all squares).
         const history = this.state.history;
         //Takes current version of the squares and checks if the someone won
         const current = history[this.state.stepNumber];
-        const winner = calculateWinner(current.squares);
+        const won = calculateWinner(current.squares);
 
         // Creates a button for every version of the squares board and 
         // shows the number of the move and the position that was clicked on (row, col)
         // the first button is used to restart the board
         // also, checks what is the current move and bolds the button that
         // corresponds it.
-        const moves = history.map((step, move) => {
+        let moves = history.map((step, move) => {
             const location = this.state.stepsLocation[move - 1];
             const desc = move ?
                 `Go to move #${move} with the location ${location}` :
@@ -163,26 +207,45 @@ class Game extends React.Component {
             );
         });
 
+        // Reverse the order of the moves components if the order is descending
+        if (this.state.ascending === false) {
+            moves = moves.map(move => move).reverse();
+        }
+
         // Shows information about the next turn or if there's a winner 
         let status;
-        if (winner) {
-            status = 'Winner: ' + winner;
+        let rowWinner = null;
+        if (won) {
+            status = 'Winner: ' + won[0];
+            rowWinner = [won[1], won[2], won[3]];
         } else {
             status = 'Next player: ' + (this.state.xIsNext ? 'X' : 'O');
         }
 
+        let enableToggle = false;
+        if (this.state.stepsLocation.length > 0) {
+            enableToggle = true;
+        }
+
         // Renders the game with the board react component and the props
         // of the squares object and onClick handler
+        // also renders the toggle button to change the order of the moves
+        // and the list of moves itself.
         return (
             <div className="game">
                 <div className="game-board">
                     <Board
                         squares={current.squares}
+                        rowWinner={rowWinner}
                         onClick={(i, j, k) => this.handleClick(i, j, k)}
                     />
                 </div>
                 <div className="game-info">
                     <div>{status}</div>
+                    <ToggleOrder
+                        enableToggle={enableToggle}
+                        handleToggleChange={() => this.handleToggleChange()}
+                    />
                     <ol>{moves}</ol>
                 </div>
             </div>
@@ -205,7 +268,7 @@ function calculateWinner(squares) {
     for (let i = 0; i < lines.length; i++) {
         const [a, b, c] = lines[i];
         if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
-            return squares[a];
+            return [squares[a], a, b, c];
         }
     }
     return null;
